17b3e942fcf3f9a696f768e2ff901a311b5ca515
Ossian <62546257+toapapper@users.noreply.github.com>
2022-09-01 14:49:17 +0200

    b

--- tinyLocalJam2/tinyLocalJam2/renderer.h
+++ tinyLocalJam2/tinyLocalJam2/renderer.h
@@ -6,6 +6,11 @@ class Point {
 public:
 	int x, y;
 
+	Point() {
+		x = 0;
+		y = 0;
+	}
+
 	Point(int x, int y) {
 		this->x = x;
 		this->y = y;
@@ -17,8 +22,16 @@ class Renderer {
 
 
 	void DrawCharacter(char character, Point position);
+	
 	void Update() {
 
 	}
+
+};
+
+class DrawCall {
+	char character;
+	Point position;
+
 
 };
