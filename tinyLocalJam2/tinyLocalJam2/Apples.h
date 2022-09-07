b2625a7386d665b28275481b17fb350c0d0e2c09
Ossian <62546257+toapapper@users.noreply.github.com>
2022-09-07 14:33:28 +0200

    OPTIMIZED
    Gjorde så man inte kollar vartenda äpple när man ska äta. Märkte att det faktiskt började lagga lite när spelet gick.
    Äpplena ligger nu istället i en "2d" array och det görs df. bara en arrayåtkomst per applecheck.

--- tinyLocalJam2/tinyLocalJam2/Apples.h
+++ tinyLocalJam2/tinyLocalJam2/Apples.h
@@ -3,56 +3,61 @@
 #include "renderer.h"
 #include <random>
 
+std::random_device seed;
+std::mt19937 randomEngine(seed());
 
 class AppleEngine {
 	int spawnDelay = 5;//ticks
 	int spawnTimer = 0;
 	Renderer& rend;
 	Point arenaSize;
-	std::uniform_int_distribution<unsigned> randX;
-	std::uniform_int_distribution<unsigned> randY;
-	std::default_random_engine aaaX;
+
+	std::uniform_int_distribution<int> randX;
+	std::uniform_int_distribution<int> randY;
 
 public:
-	std::vector<Point> apples;
+	bool* apples;
 
-	AppleEngine(Renderer& rend, Point arenaSize) :rend(rend), arenaSize() {
-			randX = std::uniform_int_distribution<unsigned>(0, arenaSize.x);
-			randX = std::uniform_int_distribution<unsigned>(0, arenaSize.x);
-			randY = std::uniform_int_distribution<unsigned>(0, arenaSize.y);
-
-			unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-			aaaX = std::default_random_engine(seed);
+	AppleEngine(Renderer& rend, Point arenaSize) :rend(rend), arenaSize(arenaSize), randX(0, arenaSize.x), randY(0, arenaSize.y)
+	{
+		apples = new bool[arenaSize.x * arenaSize.y];
+		for (int i = 0; i < arenaSize.x * arenaSize.y; i++)
+		{
+			apples[i] = false;
+		}
 	}
-	~AppleEngine(){}
+	~AppleEngine()
+	{
+		delete[] apples;
+	}
 
 	void Update() {
 		spawnTimer++;
 		if (spawnTimer >= spawnDelay) {
-			int x = randX(aaaX);
-			int y = randY(aaaX);
+			int x = randX(randomEngine);
+			int y = randY(randomEngine);
 
-			apples.push_back(Point(x, y));
+			apples[y * arenaSize.x + x] = true;
 
 			spawnTimer = 0;
 		}
 	}
 
 	void Draw() {
-		for (Point apple : apples) {
-			rend.DrawCharacter('a', apple);
+		for (int i = 0; i < arenaSize.x * arenaSize.y; i++)
+		{
+			if (apples[i])
+				rend.DrawCharacter('a', Point((i % arenaSize.x), (i - i % arenaSize.x) / arenaSize.x));
 		}
 	}
 
 	bool AppleCheck(Point position) {
-		int i = 0;
-		for (; i < apples.size(); i++) {
-			if (apples[i] == position) {
-				apples[i] = Point(-1, -1);
-				return true;
-			}
-		}
 
+		if (apples[position.y * arenaSize.x + position.x])
+		{
+			apples[position.y * arenaSize.x + position.x] = false;
+			return true;
+		}
 
 		return false;
 	}
